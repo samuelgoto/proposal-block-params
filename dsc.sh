158ab2bb7e375bea9752f5237b4ba8eb645f8bf5
Sam Goto <goto@goto-macbookair2.roam.corp.google.com>
2017-08-03 21:38:15 -0700

    connecting the command line tool with the DocScript compiler

--- dsc.sh
+++ dsc.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env node
-console.log('Hello, world!');
+
+var program = require('commander');
+var {DocScript} = require('./docscript.js');
+var fs = require('fs');
+
+program
+  .arguments('<file>')
+  // .option('-u, --username <username>', 'The user to authenticate as')
+  // .option('-p, --password <password>', 'The user\'s password')
+  .action(function(file) {
+    // console.log('user: %s pass: %s file: %s',
+    //   program.username, program.password, file);
+    // console.log(`${file}`);
+    // DocScript.compile();
+
+    fs.readFile(file, 'utf8', function(err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(DocScript.compile(data));
+    });
+  })
+  .parse(process.argv);
